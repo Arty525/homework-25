5b8586eb47c289415e4d4793f3318e76c139af0d
Андрей <newart525@yandex.ru>
2022-08-05 20:46:04 +0300

    Добавьте файлы проекта.

---
+++ EX11/src/EX11.cpp
@@ -0,0 +1,26 @@
+﻿#include "header.h"
+
+int main() {
+	std::string command;
+	std::cout << "Enter instrument: ";
+	std::cin >> command;
+	do {
+		if (command == "scalpel") {
+			scalpel();
+		}
+		else if (command == "hemostat") {
+			hemostat();
+		}
+		else if (command == "tweezers") {
+			tweezers();
+		}
+		else if (command == "suture") {
+			suture();
+		}
+		else {
+			std::cout << "Unknown command"
+		}
+	} while (scalpelStart.x != sutureStart.x && scalpelStart.y != sutureStart.y && scalpelEnd.x != sutureEnd.x && scalpelEnd.y != sutureEnd.y);
+
+	return 0;
+}
